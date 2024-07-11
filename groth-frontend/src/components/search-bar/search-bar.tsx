fc5a9e44cbc49567ba417110af99348db9999f9e
Irina Dubrovskaya <irina.dubrovskaya95@gmail.com>
2024-07-11 22:00:55 +0300

    get value from Autocomplete, create SingleAssetPage, redirect user to SingleAssetPage, redurect user back

--- groth-frontend/src/components/search-bar/search-bar.tsx
+++ groth-frontend/src/components/search-bar/search-bar.tsx
@@ -1,15 +1,23 @@
 import { Stack, Autocomplete, TextField } from '@mui/material';
 import { useAppSelector } from '../../core/store/auth/auth.selector';
 import { ISingleAsset } from '../../core/types/assets';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export const SearchBar = () => {
+  const navigate = useNavigate();
+  const [selectedItem, setSelectedItem] = useState<string | null>('');
+
   const assetsArray: ISingleAsset[] = useAppSelector(
     (state) => state.crypto.assets
   );
   return (
     <Stack spacing={2} sx={{ width: 300 }}>
       <Autocomplete
-        freeSolo
+        onChange={(event: any, value: string | null) => {
+          navigate(`single/${value}`);
+          setSelectedItem(null);
+        }}
         renderInput={(element) => (
           <TextField
             {...element}
